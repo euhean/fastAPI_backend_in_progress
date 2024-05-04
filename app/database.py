f2765f18e0ac8f1ab6f5bfda65de9ff5d5085405
euhean <ehernandeza02@gmail.com>
2024-05-04 13:39:32 +0200

    asdfg

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 import os
-DB_URL = DB_URL = os.getenv("DB_URL")
+DB_URL = os.getenv("DB_URL")
 engine = create_engine(DB_URL,echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
--- app/models.py
+++ app/models.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import relationship
 
 from .database import Base
 
+
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer,primary_key=True,index=True)
-    name = Column(String(255),index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), index=True)
     email = Column(String(255), unique=True, index=True)
